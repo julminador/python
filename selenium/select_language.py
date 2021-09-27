8c45fda40cab9a06f8905f45ef486568b341196e
julminador <juliansosa021@gmail.com>
2021-09-27 17:12:28 -0500

    Manejo de select

--- selenium/select_language.py
+++ selenium/select_language.py
@@ -11,11 +11,10 @@ class LanguageOptions(unittest.TestCase):
         driver.get('http://demo.onestepcheckout.com/')
         
     def test_sort_by(self):
-        self.driver.find_element_by_xpath('//*[@id="nav"]/ol/li[5]/a').click()
+        self.driver.find_element_by_xpath('/html/body/div/div[2]/header/div/div[3]/nav/ol/li[5]/a').click()
         exposed_options = ['Position', 'Name', 'Price']
         active_options = []
-        sort_by = Select(self.driver.find_elements_by_xpath('/html/body/div/div[2]/div[2]/div/div[2]/div[2]/div[3]/div[1]/div[1]/div/select'))  # Accede a las opciones del dropdown
-        # sort_by = Select(self.driver.find_elements_by_id('select-language'))
+        sort_by = Select(self.driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/div/div[2]/div[2]/div[3]/div[1]/div[1]/div/select'))  # Accede a las opciones del dropdown
         self.assertEqual(3, len(sort_by.options)) # Compruba la cantidad de opciones
 
         for option in sort_by.options:
@@ -30,7 +29,7 @@ class LanguageOptions(unittest.TestCase):
         # Verifica que el sitio cambio en la url del sitio contiene "name"
         self.assertTrue('order=name' in self.driver.current_url)
 
-        sort_by = Select(self.driver.find_elements_by_xpath('/html/body/div/div[2]/div[2]/div/div[2]/div[2]/div[3]/div[1]/div[1]/div/select'))  # Accede a las opciones del dropdown
+        sort_by = Select(self.driver.find_element_by_xpath('/html/body/div/div[2]/div[2]/div/div[2]/div[2]/div[3]/div[1]/div[1]/div/select'))  # Accede a las opciones del dropdown
         sort_by.select_by_index(0)
 
     def tearDown(self):
